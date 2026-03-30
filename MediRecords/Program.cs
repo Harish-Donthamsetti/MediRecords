b334a390724f5023a17d887e2989c5897f18e0a4
Manan Daxini <MananChhotalal.Daxini@cognizant.com>
2026-03-30 17:57:39 +0530

    Updated Program.cs

--- MediRecords/Program.cs
+++ MediRecords/Program.cs
@@ -7,37 +7,40 @@ using MediRecords.Repositories;
 using MediRecords.Repository.UserRepo;
 
 var builder = WebApplication.CreateBuilder(args);
-
+ 
 // Add DbContext
-builder.Services.AddDbContext<MediRecordsDbContext>(options => 
+builder.Services.AddDbContext<MediRecordsDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     )
 );
-
-builder.Services.AddScoped<IAuthRepository,AuthRepository>(); 
-builder.Services.AddScoped<IUserRepository,UserRepository>(); 
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IUserService,UserService>();
-
+ 
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+ 
 var app = builder.Build();
-
+ 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-        
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "MediRecords API v1");
-    });
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapControllers();
 }
-
+ 
 app.UseHttpsRedirection();
-app.UseAuthentication(); 
-app.UseAuthorization(); 
-
-app.MapControllers(); 
-
+ 
+// Authentication and Authorization
+app.UseAuthentication();
+app.UseAuthorization();
+ 
 app.Run();
