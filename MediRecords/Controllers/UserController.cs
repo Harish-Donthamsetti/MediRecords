c6f16d4c82af78d6f89b3b7303548cf2246ddbfa
Harish Donthamsetti <harishdonthamsetti@gmail.com>
2026-04-02 12:41:25 +0530

    Password Message for WeakPassword Updated

--- MediRecords/Controllers/UserController.cs
+++ MediRecords/Controllers/UserController.cs
@@ -24,7 +24,7 @@ namespace MediRecords.Controllers
         /// <param name="requestDto">The user registration data transfer object containing credentials and profile info.</param>
         /// <returns>Return the Success or ErrorMessage</returns>
         [HttpPost("register")]
-        // [Authorize(Roles = "Admin")]
+        // [Authorize(Roles = "Constant.Admin")]
         [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
