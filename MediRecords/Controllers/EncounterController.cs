cf798f0d2882d3a4224767686055cc9a888953c1
2480288Aditya <AdityaUmesh.Pawar@cognizant.com>
2026-04-07 11:55:06 +0530

    Update EncounterController.cs

--- MediRecords/Controllers/EncounterController.cs
+++ MediRecords/Controllers/EncounterController.cs
@@ -8,7 +8,7 @@ using Microsoft.VisualBasic;
 
 namespace MediRecords.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class EncounterController : ControllerBase
     {
