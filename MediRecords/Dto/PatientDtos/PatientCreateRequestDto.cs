660cc83ce46d12a13b143054e6bbcc49fb3edb1e
Harish Donthamsetti <harishdonthamsetti@gmail.com>
2026-04-07 16:23:50 +0530

    Create Patient API

---
+++ MediRecords/Dto/PatientDtos/PatientCreateRequestDto.cs
@@ -0,0 +1,14 @@
+using System;
+using MediRecords.Domain.Entities;
+
+namespace MediRecords.Dto.PatientDtos;
+
+public class PatientCreateRequestDto
+{
+    public string Name { get; set; } = null!;
+    public DateOnly DOB { get; set; }
+    public string Gender { get; set; } = null!;
+    public string? PhoneNo { get; set; }
+    public string? AddressJSON { get; set; }
+    public int? PrimaryProviderId { get; set; }
+}
---
+++ MediRecords/Dto/PatientDtos/PatientCreateResponseDto.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace MediRecords.Dto.PatientDtos;
+
+public class PatientCreateResponseDto
+{
+    public int PatientId { get; set; }
+}
