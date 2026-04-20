8c860ca276839405bfc36b9a1c59d5ddbedfa7d1
shivam26ce30 <Shivam.Kumar26ce30@cognizant.com>
2026-04-20 13:56:49 +0530

    removed commented code and update route

--- MediRecords/Controllers/ImagingOrderController.cs
+++ MediRecords/Controllers/ImagingOrderController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MediRecords.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ImagingOrderController : ControllerBase
     {
