f0704b35a6d90f9313028180c91ee9a891f4117f
shivam26ce30 <Shivam.Kumar26ce30@cognizant.com>
2026-04-21 09:06:09 +0530

    removed unwanted comments

--- MediRecords/Repository/ImagingOrderRepository/ImagingOrderRepository.cs
+++ MediRecords/Repository/ImagingOrderRepository/ImagingOrderRepository.cs
@@ -35,13 +35,6 @@ public class ImagingOrderRepository : IImagingOrderRepository
             throw new InvalidOperationException(Constant.EncounterClosed);
         }
 
-
-
-        // bool imagingOrderExists = await _context.ImagingOrders.AnyAsync(o =>
-        //     o.EncounterId == order.EncounterId &&
-        //     o.StudyType.Trim().ToLower() == order.StudyType.Trim().ToLower() &&
-        //     o.Status);
-
         bool imagingOrderExists = await _context.ImagingOrders.AnyAsync(o => o.EncounterId == order.EncounterId && o.StudyType == order.StudyType && o.Status);
 
 
