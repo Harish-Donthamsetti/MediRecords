22e7d5f0da0ff44562a1be723e16cb7aafc0406a
Manan Daxini <MananChhotalal.Daxini@cognizant.com>
2026-04-22 09:49:46 +0530

    Added Enum Classes For Required Files

---
+++ MediRecords.Domain/Enums/UserRoleEnums.cs
@@ -0,0 +1,17 @@
+using System.Runtime.Serialization;
+
+namespace MediRecords.Domain.Enums;
+
+public enum UserRoleEnums
+{
+    [EnumMember(Value = "Admin")]
+    Admin,
+    [EnumMember(Value = "Physician")]
+    Physician,
+    [EnumMember(Value = "Nurse")]
+    Nurse,
+    [EnumMember(Value = "LabTechnician")]
+    LabTechnician,
+    [EnumMember(Value = "FrontDesk")]
+    FrontDesk
+}
